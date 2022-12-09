13598f1f54ab315d66456dd8f1262ae3bcad648c
Alessandro Capozzi <alessandro.capozzi@qwentes.it>
2022-12-09 11:19:13 +0100

    feat(dashboard): skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainNavComponent } from './features/shell/components/sidebar/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { UserProfileComponent } from './features/shell/components/user-profile/user-profile.component';
+import { DashboardComponent } from './features/shell/features/coachupper/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, MainNavComponent],
+  declarations: [
+    AppComponent,
+    MainNavComponent,
+    UserProfileComponent,
+    DashboardComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
